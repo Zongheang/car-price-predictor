372b6fb413435801acb384b92de26308f8dd06c6
Oeng Songhieng <songheangung@gmail.com>
2025-06-06 15:28:28 +0700

    Update port

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ from flask_cors import CORS, cross_origin
 import pickle
 import pandas as pd
 import numpy as np
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -82,4 +83,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Railway port or default 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
